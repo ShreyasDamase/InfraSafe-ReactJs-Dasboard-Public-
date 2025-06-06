f764c23be00531773fb811c51d65996ea0246af5
Shreyas Damase <113159691+ShreyasDamase@users.noreply.github.com>
2025-06-06 16:34:12 +0530

    Update firebaseAuth.tsx

--- src/firebaseAuth/firebaseAuth.tsx
+++ src/firebaseAuth/firebaseAuth.tsx
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhtILOlyaTHB1jzQGPtBuE50J6nj1qgC0",
-  authDomain: "infrasafe-8b834.firebaseapp.com",
-  projectId: "infrasafe-8b834",
-  messagingSenderId: "793190604982",
-  appId: "1:793190604982:web:c33a7ce655d48e8dad8617",
-  measurementId: "G-6ZNNKQBKCV",
+  apiKey: "<YOUR_FIREBASE_API_KEY>",
+  authDomain: "<YOUR_FIREBASE_AUTH_DOMAIN>",
+  projectId: "<YOUR_FIREBASE_PROJECT_ID>",
+  messagingSenderId: "<YOUR_FIREBASE_MESSAGING_SENDER_ID>",
+  appId: "<YOUR_FIREBASE_APP_ID>",
+  measurementId: "<YOUR_FIREBASE_MEASUREMENT_ID>",
 };
 
 const app = initializeApp(firebaseConfig);
